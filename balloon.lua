191441cfb26e8e3a6e314d7338c2c6a32b225812
AlessandroR9 <45742965+AlessandroR9@users.noreply.github.com>
2019-01-02 16:43:53 +0100

    Update balloon.lua

--- balloon.lua
+++ balloon.lua
@@ -1,7 +1,4 @@
--- Module/class for platformer enemy
--- Use this as a template to build an in-game enemy
-
--- Define module
+-- Definizione del modulo
 local M = {}
 
 local composer = require( "composer" )
@@ -10,12 +7,10 @@ function M.new( parent, x, y )
 
 	if not parent then error( "ERROR: Expected display object" ) end
 
-	-- Get scene and sounds
+	-- Crea la scena ed i suoni
 	local scene = composer.getScene( composer.getSceneName( "current" ) )
-	--local sounds = scene.sounds
-	-- Store map placement and hide placeholder
 
-	-- Load spritesheet
+	-- Carica lo spritesheet 
 	local sheetData = { width = 50, height = 50, numFrames = 9, sheetContentWidth = 450, sheetContentHeight = 50 }
 	local sheet = graphics.newImageSheet( "balloonSheet.png", sheetData )
 	local sequenceData = {
@@ -28,66 +23,7 @@ function M.new( parent, x, y )
 	balloon:setSequence("balloon")
 	balloon:play()
 
-
-	-- -- Add physics
-	-- physics.addBody( instance, "dynamic", { radius = 350, density = 1, bounce = 0.1, friction =  1.0 } )
-	-- instance.isFixedRotation = true
-	-- instance.anchorY = 0.77
-	-- instance.angularDamping = 3
-	-- instance.isDead = false
-	--
-	-- function instance:die()
-	-- 	--audio.play( sounds.sword )
-	-- 	self.isFixedRotation = false
-	-- 	self.isSensor = true
-	-- 	self:applyLinearImpulse( 0, -2 )
-	-- 	self.isDead = true
-	-- end
-	--
-
-	--
-	-- local max, direction, flip, timeout = 250, 5000, 0.133, 0
-	-- direction = direction * ( ( instance.xScale < 0 ) and 1 or -1 )
-	-- flip = flip * ( ( instance.xScale < 0 ) and 1 or -1 )
-	--
-	-- local function enterFrame()
-	--
-	-- 	-- Do this every frame
-	-- 	local vx, vy = instance:getLinearVelocity()
-	-- 	local dx = direction
-	-- 	if instance.jumping then dx = dx / 4 end
-	-- 	if ( dx < 0 and vx > -max ) or ( dx > 0 and vx < max ) then
-	-- 		instance:applyForce( dx or 0, 0, instance.x, instance.y )
-	-- 	end
-	--
-	-- 	-- Bumped
-	-- 	if math.abs( vx ) < 1 then
-	-- 		timeout = timeout + 1
-	-- 		if timeout > 30 then
-	-- 			timeout = 0
-	-- 			direction, flip = -direction, -flip
-	-- 		end
-	-- 	end
-	--
-	-- 	-- Turn around
-	-- 	instance.xScale = math.min( 1, math.max( instance.xScale + flip, -1 ) )
-	-- end
-	--
-	-- function instance:finalize()
-	-- 	-- On remove, cleanup instance, or call directly for non-visual
-	-- 	Runtime:removeEventListener( "enterFrame", enterFrame )
-	-- 	instance = nil
-	-- end
-	--
-	-- -- Add a finalize listener (for display objects only, comment out for non-visual)
-	-- instance:addEventListener( "finalize" )
-	--
-	-- -- Add our enterFrame listener
-	-- Runtime:addEventListener( "enterFrame", enterFrame )
-	--
-	-- -- Add our collision listener
-
-	-- Return instance
+	-- Restituisce l'istanza "balloon"
 	balloon.name = "balloon"
 	balloon.type = "balloon"
 	return balloon
