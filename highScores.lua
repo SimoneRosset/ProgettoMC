030786aac901fde951d7b89998b646b84ba475ec
AlessandroR9 <45742965+AlessandroR9@users.noreply.github.com>
2019-01-08 16:20:04 +0100

    Update highScores.lua

--- highScores.lua
+++ highScores.lua
@@ -7,23 +7,14 @@ local clouds = require "cloud"
 local balloon = require "balloon"
 local click = audio.loadSound( "click.wav" )
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
-
 
 -- Initialize variables
 local json = require( "json" )
-
 local scoresTable = {}
-
 local filePath = system.pathForFile( "scores.json", system.DocumentsDirectory )
 
 local function loadScores()
-
     local file = io.open( filePath, "r" )
-
     if file then
         local contents = file:read( "*a" )
         io.close( file )
@@ -35,14 +26,13 @@ local function loadScores()
     end
 end
 
-local function saveScores()
 
+local function saveScores()
     for i = #scoresTable, 4, -1 do
         table.remove( scoresTable, i )
     end
 
     local file = io.open( filePath, "w" )
-
     if file then
         file:write( json.encode( scoresTable ) )
         io.close( file )
@@ -55,17 +45,14 @@ end
 
 
 function scene:create( event )
-
     local sceneGroup = self.view
-
-
     -- Code here runs when the scene is first created but has not yet appeared on screen
     -- Load the previous scores
-        loadScores()
-        -- Insert the saved score from the last game into the table, then reset it
-   table.insert( scoresTable, composer.getVariable( "finalScore" ) )
-   composer.setVariable( "finalScore", 0 )
-   -- Sort the table entries from highest to lowest
+    loadScores()
+    -- Insert the saved score from the last game into the table, then reset it
+    table.insert( scoresTable, composer.getVariable( "finalScore" ) )
+    composer.setVariable( "finalScore", 0 )
+    -- Sort the table entries from highest to lowest
     local function compare( a, b )
         return a > b
     end
@@ -74,92 +61,75 @@ function scene:create( event )
     saveScores()
     composer.setVariable( "record", scoresTable[1] )
 
-
     local background = display.newImageRect( sceneGroup, "background.png", 800, 1400 )
-   background.x = display.contentCenterX
-   background.y = display.contentCenterY
-   background:setFillColor( 0.9)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+    background:setFillColor( 0.9)
 
-   grass = display.newImageRect( sceneGroup, "ground.png", screenW, screenH)
-  grass.anchorX = 0
-  grass.anchorY = 1
-  --  draw the grass at the very bottom of the screen
-  grass.x, grass.y = display.screenOriginX, display.actualContentHeight*1.8 + display.screenOriginY
-   grass.alpha=0.7
-   for _=1,3 do
-     clouds.new(sceneGroup, math.random(display.actualContentWidth), math.random(0, display.actualContentHeight/4.5))
-
-   end
-   local highScoresHeader = display.newText( sceneGroup, "highScores:", display.contentCenterX, 50, native.systemFont, 44 )
-   highScoresHeader:setFillColor(0,0,0)
-   for i = 1, 3 do
+    grass = display.newImageRect( sceneGroup, "ground.png", screenW, screenH)
+    grass.anchorX = 0
+    grass.anchorY = 1
+    --  draw the grass at the very bottom of the screen
+    grass.x, grass.y = display.screenOriginX, display.actualContentHeight*1.8 + display.screenOriginY
+    grass.alpha=0.7
+    for _=1,3 do
+        clouds.new(sceneGroup, math.random(display.actualContentWidth), math.random(0, display.actualContentHeight/4.5))
+    end
+    
+    local highScoresHeader = display.newText( sceneGroup, "highScores:", display.contentCenterX, 50, native.systemFont, 44 )
+    highScoresHeader:setFillColor(0,0,0)
+    
+    for i = 1, 3 do
            if ( scoresTable[i] ) then
-               local yPos = 60 + ( i * 56 )
-
-               local rankNum = display.newText( sceneGroup, i .. ") ".. scoresTable[i], display.contentCenterX, yPos, native.systemFont, 30 )
-                   rankNum:setFillColor( 0 )
-
-
-           end
-       end
-
-       local function onBackBtnRelease()
-       	-- go to game.lua scene
+                local yPos = 60 + ( i * 56 )
+                local rankNum = display.newText( sceneGroup, i .. ") ".. scoresTable[i], display.contentCenterX, yPos, native.systemFont, 30 )
+                rankNum:setFillColor( 0 )
+            end
+    end
+    
+    local function onBackBtnRelease()
+        -- go to game.lua scene
         audio.play(click)
-
-       	composer.removeScene( "highScores")
+        composer.removeScene( "highScores")
        	composer.gotoScene( "menu", "fade", 400 )
+        return true	-- indicates successful touch
+    end
 
-       	return true	-- indicates successful touch
-       end
+    local backBtn = widget.newButton{
+        textOnly=true,
+        label="back",
+        labelColor = { default={ 0, 0, 0 }, over={ 1, 1, 1, 1 } },
+        default="button.png",
+        over="button-over.png",
+        width=200, height=50,
+        onRelease = onBackBtnRelease	-- event listener function
+    }
+    backBtn.x = display.contentCenterX*0.2
+    backBtn.y = display.contentCenterY-display.contentCenterY
+    sceneGroup:insert( backBtn )
 
-       local backBtn = widget.newButton{
-         textOnly=true,
-         label="back",
-         labelColor = { default={ 0, 0, 0 }, over={ 1, 1, 1, 1 } },
-         default="button.png",
-         over="button-over.png",
-         width=200, height=50,
-         onRelease = onBackBtnRelease	-- event listener function
-       }
-       backBtn.x = display.contentCenterX*0.2
-       backBtn.y = display.contentCenterY-display.contentCenterY
-       sceneGroup:insert( backBtn )
-
-       local balloon=balloon.new(sceneGroup, display.contentCenterX,  display.contentCenterY*1.3)
-
-     	corda=display.newImageRect( sceneGroup, "corda.png", 1,grass.y-grass.height-balloon.y)
-     
-     corda.x,corda.y=balloon.x,balloon.y+balloon.height
-     corda.alpha=0.4
-
-     balloon:toFront()
-
+    local balloon=balloon.new(sceneGroup, display.contentCenterX,  display.contentCenterY*1.3)
+    corda=display.newImageRect( sceneGroup, "corda.png", 1,grass.y-grass.height-balloon.y)
+    corda.x,corda.y=balloon.x,balloon.y+balloon.height
+    corda.alpha=0.4
+    balloon:toFront()
 end
 
 
 function scene:show( event )
-
     local sceneGroup = self.view
     local phase = event.phase
-
+    
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
-
-
-
-
-
+    
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
-
-
     end
 end
 
 
 function scene:hide( event )
-
     local sceneGroup = self.view
     local phase = event.phase
 
@@ -168,20 +138,15 @@ function scene:hide( event )
         composer.removeScene( "highScores" )
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
-
-
     end
 end
 
 
 function scene:destroy( event )
-
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
-audio.dispose(click)
-click=nil
-
-
+    audio.dispose(click)
+    click=nil
 end
 
 
