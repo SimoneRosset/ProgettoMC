738f941ec1726923aa3920defa93ed5d5ecc4a6f
AlessandroR9 <45742965+AlessandroR9@users.noreply.github.com>
2019-01-10 11:54:03 +0100

    Modifiche per gestire suono

--- main.lua
+++ main.lua
@@ -12,3 +12,13 @@ local composer = require "composer"
 
 -- load menu screen
 composer.gotoScene( "menu" )
+composer.setVariable("sound",true)
+sound = composer.getVariable("sound")
+
+if (audio.isChannelPlaying(1)) then
+	sound = true
+end
+
+if (audio.isChannelPlaying(1) == false) then
+	sound = false
+end
